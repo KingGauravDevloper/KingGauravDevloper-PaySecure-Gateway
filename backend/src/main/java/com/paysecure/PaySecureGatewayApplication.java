f78d16a34bfe09fb2fd7324e2f886ae6d501425c
KingGauravDeveloper <kinggaurav.official@gmail.com>
2025-11-12 15:05:41 +0530

    trial

---
+++ backend/src/main/java/com/paysecure/PaySecureGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.paysecure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaySecureGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaySecureGatewayApplication.class, args);
+	}
+
+}
